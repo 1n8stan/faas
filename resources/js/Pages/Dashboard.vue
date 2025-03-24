bff89322d26ee026945607a0defe5ab7f5a44daf
DigiManila <digitizationmanila@gmail.com>
2025-03-24 15:15:29 +0800

    add daisy ui

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -15,8 +15,18 @@ import { Head } from "@inertiajs/vue3";
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">testing 101</div>
+                <div class="mockup-browser border-base-300 border w-full">
+                    <div class="mockup-browser-toolbar">
+                        <div class="input">https://daisyui.com</div>
+                    </div>
+                    <div
+                        class="grid place-content-center border-t border-base-300 h-80"
+                    >
+                        Hello!
+                    </div>
+                </div>
+                <div class="p-6 bg-primary text-primary-content rounded">
+                    This should have a themed background!
                 </div>
             </div>
         </div>
